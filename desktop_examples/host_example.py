794d6886a877e4cbcee8052b694c011e27c0d715
Jeremy Magland <jeremy.magland@gmail.com>
2019-09-20 16:09:11 -0400

    add host_example.py

---
+++ desktop_examples/host_example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import ccm_widgets as cw
+cw.init_electron()
+
+
+def main():
+    props = {
+        "vtp_path": "sha1://22f242215ed2625db02b3a1f9d89364defa013f6/quas3.vtp",
+        "download_from": "spikeforest.public",
+        "scalar_info": {
+            "name": "value",
+            "component": 0
+        },
+        "vector_field_info": {
+            "name": "value",
+            "components": [
+                    0,
+                    1,
+                    2
+            ]
+        },
+        "preset_color_map_name": "erdc_rainbow_bright",
+        "arrow_subsample_factor": 250,
+        "arrow_width": 0.3,
+        "arrow_length_factor": 0.3
+    }
+    W = cw.Surface3dVtp(**props)
+    W.host(port=6065)
+
+
+if __name__ == '__main__':
+    main()
